c0eb22e17ab2b6c3f12920d851f7baeb7fd08099
pXius <fan.engelbrecht@gmail.com>
2020-11-15 22:08:28 +0100

    navbar responsive design updated. Product page updated.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 port = process.env.PORT || 80;
-app.listen(port);
+app.listen(3000);
 
 app.get('/', (request, response) => {
   response.render('index', { title: 'Home' });
