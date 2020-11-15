12653411de8ffc89bc41cd58ea85ff9019021164
pXius <fan.engelbrecht@gmail.com>
2020-11-15 22:37:21 +0100

    port update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 port = process.env.PORT || 80;
-app.listen(3000);
+app.listen(port);
 
 app.get('/', (request, response) => {
   response.render('index', { title: 'Home' });
